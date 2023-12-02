843aaa8efd885aa82f27fa45dba552573575bc33
Violeta Malinova <violeta@mlnova.com>
2023-12-02 08:29:10 +0200

    2 December part 1

---
+++ dec2/dec2.py
@@ -0,0 +1,38 @@
+
+lines = []
+with open("test.txt") as f:
+    lines = f.read().splitlines()
+
+max_cubes = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+sum = 0
+
+for line in lines:
+    semicolon_split = line.split(":")
+
+    gameid = semicolon_split[0].split(' ')[1]
+
+    game_draws = semicolon_split[1].split(';')
+    possible = True
+
+    for game_draw in game_draws:
+        truncated = game_draw.strip()
+
+        cube_types = truncated.split(", ")
+
+        for cube_type in cube_types:
+            splitted = cube_type.split(' ')
+            count = int(splitted[0])
+            colour = splitted[1]
+
+            if count > max_cubes[colour]:
+                possible = False
+
+    if possible:
+        sum += int(gameid)
+
+print(sum)
