cee08e56539545b0ac9e2961d465cbaf7ae7f833
Violeta Malinova <violeta@mlnova.com>
2023-12-06 11:27:13 +0200

    6 December 2nd solution refactor

--- dec6/dec6-2ndsol.py
+++ dec6/dec6-2ndsol.py
@@ -6,9 +6,10 @@ with open("test.txt") as f:
 
 time = int(''.join(lines[0].split(":")[1].split()))
 distance = int(''.join(lines[1].split(":")[1].split()))
+D = math.sqrt(time**2 - 4*distance)
 
-x1 = (- time + math.sqrt(time**2 - 4*distance)) / (-2)
-x2 = (- time - math.sqrt(time**2 - 4*distance)) / (-2)
+x1 = (- time + D) / (-2)
+x2 = (- time - D) / (-2)
 
 x1 = math.ceil(x1)
 x2 = math.floor(x2)
