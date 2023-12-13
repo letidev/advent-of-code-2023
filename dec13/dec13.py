3ad59c3249628afbb34ef866d8b85a880ac715ed
Violeta Malinova <violeta@mlnova.com>
2023-12-13 11:10:04 +0200

    13 December part 2

--- dec13/dec13.py
+++ dec13/dec13.py
@@ -20,44 +20,83 @@ def columns_match(grid, left, right):
     return True
 
 
+def flip(type):
+    return "#" if type == "." else "."
+
+
+def has_horizontal_smudge(up, down):
+    smudges = []
+
+    for i in range(len(up)):
+        if up[i] != down[i]:
+            smudges.append(i)
+
+    return False if len(smudges) != 1 else True
+
+
+def has_vertical_smudge(grid, left, right):
+    smudges = []
+
+    for i in range(len(grid)):
+        if grid[i][left] != grid[i][right]:
+            smudges.append(i)
+
+    return False if len(smudges) != 1 else True
+
+
+def print_grid(grid):
+    for line in grid:
+        print(line)
+    print()
+
+
 for grid in grids:
     n = len(grid)  # rows
     m = len(grid[0])  # cols
 
-    # search for horizontal match:
+    # print_grid(grid)
+
+    ### FIX LINE SMUDGE AND SUM ###
     for i in range(n - 1):
-        if grid[i] == grid[i+1]:
-            up = i-1
-            down = i+2
-            pefrect_match = True
+        up = i
+        down = i+1
+        has_smudge = False
+        match = True
 
-            while up >= 0 and down < n:
-                if (grid[up] != grid[down]):
-                    pefrect_match = False
-                    break
+        while up >= 0 and down < n:
+            if not has_smudge and has_horizontal_smudge(grid[up], grid[down]):
+                has_smudge = True
 
-                up -= 1
-                down += 1
+            elif grid[up] != grid[down]:
+                match = False
+                break
 
-            if pefrect_match:
-                sum += (i + 1) * 100
+            up -= 1
+            down += 1
 
-    # search for vertical match:
+        if has_smudge and match:
+            sum += (i+1) * 100
+            break
+
+    ### FIX COLUMN SMUDGE AND SUM ###
     for j in range(m - 1):
-        if columns_match(grid, j, j+1):
-            left = j-1
-            right = j+2
-            pefrect_match = True
+        left = j
+        right = j + 1
+        has_smudge = False
+        match = True
 
-            while left >= 0 and right < m:
-                if not columns_match(grid, left, right):
-                    pefrect_match = False
-                    break
+        while left >= 0 and right < m:
+            if not has_smudge and has_vertical_smudge(grid, left, right):
+                has_smudge = True
+            elif not columns_match(grid, left, right):
+                match = False
+                break
 
-                left -= 1
-                right += 1
+            left -= 1
+            right += 1
 
-            if pefrect_match:
-                sum += j + 1
+        if has_smudge and match:
+            sum += j + 1
+            break
 
 print(sum)
